1c2dce2a637d0ad8b0dc250db4dcfbfcf134d9f4
davemachado <dave@davemachado.com>
2017-10-20 16:51:26 -0400

    Intial LoginView

---
+++ app/src/main/java/com/stocks/stocks_io/POJO/BaseTokenInfo.java
@@ -0,0 +1,20 @@
+package com.stocks.stocks_io.POJO;
+
+public class BaseTokenInfo {
+    private String token;
+
+    public BaseTokenInfo() {
+    }
+
+    public BaseTokenInfo(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
